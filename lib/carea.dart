1e19fb33df104aaf1a4754f216dfd1d4ccb12659
Yczar <babalolagbogo@gmail.com>
2022-06-20 18:58:24 +0100

    created a new file that houses the material app

---
+++ lib/carea.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Carea extends StatelessWidget {
+  const Carea({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Scaffold(),
+    );
+  }
+}
